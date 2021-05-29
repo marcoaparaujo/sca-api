2a297278edb767602bd35a5b940a95efe1d1206b
Marco Araujo <marcoaparaujo@gmail.com>
2021-05-29 12:52:25 -0300

    first commit

---
+++ src/main/java/com/example/scaapi/ScaapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.scaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScaapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scaapi/ScaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
