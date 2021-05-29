ebbf2e5314808053de1d4819a1176baabcbf953a
Marco Araujo <marcoaparaujo@gmail.com>
2021-05-29 13:27:58 -0300

    first commit

---
+++ src/main/java/com/example/scaapi/model/entity/Curso.java
@@ -0,0 +1,23 @@
+package com.example.scaapi.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+}
