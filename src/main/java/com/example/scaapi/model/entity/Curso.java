29dc1f82a7377cc8f8767c8136703bbb77547454
Marco Araújo <marcoaparaujo@gmail.com>
2021-08-30 00:41:16 -0300

    atributo inverso curso

--- src/main/java/com/example/scaapi/model/entity/Curso.java
+++ src/main/java/com/example/scaapi/model/entity/Curso.java
@@ -1,13 +1,12 @@
 package com.example.scaapi.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Data
@@ -20,4 +19,8 @@ public class Curso {
     private Long id;
 
     private String nome;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "curso")
+    private List<Disciplina> disciplinas;
 }
